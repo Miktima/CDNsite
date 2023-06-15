c23428cc500e8b1228a8256873289d057a302d6f
Тимофеев Михаил Константинович <m.timofeev@ria.ru>
2023-06-15 16:03:47 +0300

    to deploy

--- cdn/cdn/urls.py
+++ cdn/cdn/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('stat_cdnnow/', include('stat_cdnnow.urls')),
@@ -27,4 +29,4 @@ urlpatterns = [
     path('clear_cache/', include('clear_cache.urls')),
     path('', views.index, name='main'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
